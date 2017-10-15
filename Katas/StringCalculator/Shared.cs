e76bd3cc7c15e6ffad95e121597e87cfb729d1dc
Marcin Pepliński <pepliniakmarcin@gmail.com>
2017-10-15 18:57:35 +0200

    second commit

---
+++ Katas/StringCalculator/Shared.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Shared
+    {
+
+        public int Add(string number)
+        {
+            if (string.IsNullOrEmpty(number) ||string.IsNullOrEmpty(number.Trim()))
+            {
+                return 0;              
+            }
+            else
+            {
+                number = number.Replace("\n", ",");
+                int returnValue = 0;
+                string[] pisesString = number.Split(',');
+                foreach (string pieace in pisesString)
+                {
+                    if (!string.IsNullOrEmpty(pieace) && !string.IsNullOrEmpty(pieace.Trim()))
+                    {
+                        int pieceInt = 0;
+                        if (int.TryParse(pieace, out pieceInt))
+                        {
+                            returnValue += pieceInt;
+                        }
+                    }
+                }
+
+                return returnValue;
+            }
+            
+        }
+
+
+    }
+}
---
+++ Katas/StringCalculatorTest/StringCalculatorTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+        public void CheckReturnValueOne()
+        {
+            Shared shared = new Shared();
+            int returnValue = shared.Add("1");
+            Assert.AreEqual(returnValue, 1);
+        }
+
+        [TestMethod]
+        public void CheckReturnValueForTwoValueAndComa()
+        {
+            Shared shared = new Shared();
+            int returnValue = shared.Add("1,2");
+            Assert.AreEqual(returnValue, 3);
+        }
+
+        [TestMethod]
+        public void CheckReturnValueForNullString()
+        {
+            Shared shared = new Shared();
+            int returnValue = shared.Add(null);
+            Assert.AreEqual(returnValue, 0);
+        }
+
+        [TestMethod]
+        public void CheckReturnValueForEmptyString()
+        {
+            Shared shared = new Shared();
+            int returnValue = shared.Add(string.Empty);
+            Assert.AreEqual(returnValue, 0);
+        }
+
+        [TestMethod]
+        public void CheckReturnValueForSpacebare()
+        {
+            Shared shared = new Shared();
+            int returnValue = shared.Add("  ");
+            Assert.AreEqual(returnValue, 0);
+        }
+
+        [TestMethod]
+        public void CheckReturnValueForHole()
+        {
+            Shared shared = new Shared();
+            int returnValue = shared.Add(",1,2,3,,4,,5,5");
+            Assert.AreEqual(returnValue, 20);
+        }
+
+        [TestMethod]
+        public void CheckReturnValueForN()
+        {
+            Shared shared = new Shared();
+            int returnValue = shared.Add(",1\n2,3\n,,4,,5\n,5");
+            Assert.AreEqual(returnValue, 20);
+        }
+
+
+    }
+}
